1f565447ea01e3c566e4de3f8cefacd27eaf0d54
wirhanfr <63827880+wirhanfr@users.noreply.github.com>
2023-09-26 08:42:24 +0700

    Penambahan Case Sensitive

---
+++ case_sensitivity/helloPHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    echo "Hello PHP<br>";
+    echo "Hello PHP<br>";
+    echo "Hello PHP<br>";
+    ?>
+
+</body>
+
+</html>
---
+++ case_sensitivity/variabel.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $name = "tubagus";
+    echo "Nama saya " . $name . "<br>";
+    echo "Nama saya " . $NAME . "<br>";
+    ?>
+</body>
+
+</html>
